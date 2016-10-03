7c414fbdcc12677147fb86a40e48cc442234890e
Merad <leonel-merad@gmx.us>
2016-10-03 09:40:28 +0200

    Try

---
+++ src/main/java/edu/insightr/spellmonger/Ritual.java
@@ -0,0 +1,34 @@
+package edu.insightr.spellmonger;
+
+/**
+ * Created by Merad on 03/10/2016.
+ */
+public class Ritual {
+
+    private final String type;
+    private final int value;
+
+    public Ritual(String type){
+        this.type=type;
+
+        switch(type) {
+            case "Curse":
+                value = -3;
+                break;
+            case "Blessing":
+                value = 3;
+                break;
+            default:
+                System.out.println("This is not a ritual.");
+                value = 0;
+                break;
+        }
+    }
+
+    public String getType(){
+        return this.type;
+    }
+    public int getValue(){
+        return this.value;
+    }
+}
