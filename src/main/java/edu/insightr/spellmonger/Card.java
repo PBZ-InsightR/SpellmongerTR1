c43e3ced8cc2df36e0b1a861f1f1b2c212c76120
Chloé JARTOUX <chlo jartoux>
2016-10-04 22:44:22 +0200

    Correction de la fonction mix() pour avoir des rituels.

--- src/main/java/edu/insightr/spellmonger/Card.java
+++ src/main/java/edu/insightr/spellmonger/Card.java
@@ -5,13 +5,20 @@ package edu.insightr.spellmonger;
  */
 
 public abstract class Card {
-    private final String id;
+    private String id;
 
-    public Card(String id){
+    public Card(String id) {
         this.id = id;
     }
 
-    public String getId(){
+    public String getId() {
         return id;
     }
+
+
+
+
+
+
+
 }
