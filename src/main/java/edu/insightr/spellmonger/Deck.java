7f1687da184a4d84701960d6b0ce27fdb62b923e
Dam's <dam.rubal@hotmail.com>
2016-10-14 11:34:59 +0200

    Deck_Dam

--- src/main/java/edu/insightr/spellmonger/Deck.java
+++ src/main/java/edu/insightr/spellmonger/Deck.java
@@ -12,9 +12,9 @@ class Deck {
     private List<Card> l_carte;
     private List<Card> la_main;
 
-    Deck() {
+    Deck(){
 
-        l_carte = new ArrayList<>(40);
+        l_carte= new ArrayList<>(40);
         mix();
     }
 
@@ -23,8 +23,8 @@ class Deck {
     {
         List<String> ListPourMelanger = new ArrayList<>();
 
-        int compteur = 1;
-        int max_size = 0;
+        int compteur=1;
+        int max_size=0;
 
         ListPourMelanger.add("Eagle");
         ListPourMelanger.add("Wolf");
@@ -35,30 +35,35 @@ class Deck {
 
         // On prépare une table pour la vérification future
         HashMap<String, Integer> hashmap = new HashMap<>();
-        for (String s : ListPourMelanger) {
-            hashmap.put(s, 0);
+        for(String s: ListPourMelanger)
+        {
+            hashmap.put(s,0);
         }
 
         do {
 
 
-            String carte_nom = ListPourMelanger.get(NombreAleatoire(0, ListPourMelanger.size() - 1));
+            String carte_nom = ListPourMelanger.get(NombreAleatoire(0,ListPourMelanger.size()-1));
             //System.out.println(carte_nom);
-            if (EstUneCreature(carte_nom)) {
-                Card new_carte = new Creature(carte_nom);
-                l_carte.add(new_carte);
+            if(EstUneCreature(carte_nom))
+            {
+                Card new_carte= new Creature(carte_nom);
+                  l_carte.add(new_carte);
 
                 // Chaque Créature doit être presente au maximum 10 fois .
                 max_size = 10;
-                hashmap.put(carte_nom, hashmap.get(carte_nom) + 1);
-                if (hashmap.get(carte_nom) == max_size) {
+                hashmap.put(carte_nom, hashmap.get(carte_nom)+1);
+                if(hashmap.get(carte_nom) == max_size)
+                {
                     // on l'enleve du deck
                     ListPourMelanger.remove(carte_nom);
                 }
-            } else {
-                Card new_carte = new Ritual(carte_nom);//choisit au hasard une carte.
+            }
+            else
+            {
+                Card new_carte= new Ritual(carte_nom);//choisit au hasard une carte.
                 l_carte.add(new_carte);
-                // Les rituels ne sont pas tous present en nombre identique.
+            // Les rituels ne sont pas tous present en nombre identique.
                 if (new_carte.getId().equals("Curse")) {
                     max_size = 2;
                     hashmap.put(carte_nom, hashmap.get(carte_nom) + 1);
@@ -69,42 +74,46 @@ class Deck {
                 } else if (new_carte.getId().equals("Blessing")) {
                     max_size = 3;
                     hashmap.put(carte_nom, hashmap.get(carte_nom) + 1);
-                    if (hashmap.get(carte_nom) == max_size) {
-
-                        ListPourMelanger.remove(carte_nom);
-                    } else {
-                        max_size = 5;
-                        hashmap.put(carte_nom, hashmap.get(carte_nom) + 1);
                         if (hashmap.get(carte_nom) == max_size) {
 
                             ListPourMelanger.remove(carte_nom);
                         }
-                    }
+                    else {
+                        max_size = 5;
+                        hashmap.put(carte_nom, hashmap.get(carte_nom) + 1);
+                            if (hashmap.get(carte_nom) == max_size) {
+
+                                ListPourMelanger.remove(carte_nom);
+                            }
+                        }
                 }
             }
 
             compteur++;
 
 
-        } while (compteur < 40);
+
+        }while(compteur<40);
 
 
     }
 
-    private static boolean EstUneCreature(String s) {
+    private static boolean EstUneCreature(String s)
+    {
         boolean res = false;
-        if (s.equals("Eagle") || s.equals("Wolf") || s.equals("Bear")) {
+        if(s.equals("Eagle") || s.equals("Wolf") ||s.equals("Bear"))
+        {
             res = true;
         }
 
         return res;
     }
 
-    private int NombreAleatoire(int min, int max) // Renvoi un nombre aléatoire entre les bornes choisies
+    private int NombreAleatoire(int min , int max ) // Renvoi un nombre aléatoire entre les bornes choisies
     {
         Random rand = new Random();
 
-        return rand.nextInt(max - min + 1) + min; //Formule mathematique
+        return rand.nextInt(max-min+1)+min; //Formule mathematique
 
     }
 
@@ -122,6 +131,7 @@ class Deck {
             la_main=null;
         }
         return la_main;
+
 
     }
 
