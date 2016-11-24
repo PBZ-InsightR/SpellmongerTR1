5cf65f139877915c05658a87ecc343d1c583a330
Ken Moussat <ken.moussat@devinci.fr>
2016-11-24 09:54:15 +0100

    Adding Tests

---
+++ src/edu/insightr/spellmonger/Test/MainTest.groovy
@@ -0,0 +1,7 @@
+package edu.insightr.spellmonger.Test
+
+/**
+ * Created by Ken on 24/11/2016.
+ */
+class MainTest extends GroovyTestCase {
+}
---
+++ src/edu/insightr/spellmonger/Test/MainTest1.groovy
@@ -0,0 +1,23 @@
+package edu.insightr.spellmonger.Test
+
+/**
+ * Created by Ken on 24/11/2016.
+ */
+class MainTest1 extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void tearDown() {
+
+    }
+
+    void testStart() {
+
+    }
+
+    void testMain() {
+
+    }
+}
