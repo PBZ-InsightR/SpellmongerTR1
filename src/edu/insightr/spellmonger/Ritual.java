7359787b232a324ad686b705c2763ce9efe6ab0a
crendu <rendu.clement@gmail.com>
2016-12-05 00:48:10 +0100

    mise en forme

--- src/edu/insightr/spellmonger/Ritual.java
+++ src/edu/insightr/spellmonger/Ritual.java
@@ -1,45 +1,37 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package edu.insightr.spellmonger;
 
-
 public class Ritual extends Card {
-    private int value;
-    private boolean shield=false;
-
-
-    public Ritual(String id) {
-        super(id);
-        final String CurseCard = "Curse";
-        final String ShieldCard = "Shield";
-        final String BlessingCard = "Blessing";
-
-        switch(id) {
-            case CurseCard:
-                this.value = 3;
-                break;
-
-            case ShieldCard:
-                this.shield=true;
-                this.value=0;
-                break;
-
-            case BlessingCard:
-                this.value = -3;
-                break;
-
-            default:
-                System.out.println("This is not a ritual.");
-                this.value = 0;
-        }
-
-    }
-
-    int getValue() {
-        return this.value;
-    }
-    boolean getShield(){return this.shield;}
+	private int value;
+	private boolean shield=false;
+	
+	public Ritual(String id) {
+		super(id);
+		final String CurseCard = "Curse";
+		final String ShieldCard = "Shield";
+		final String BlessingCard = "Blessing";
+		
+		switch(id) {
+			case CurseCard:
+				this.value = 3;
+				break;
+				
+			case ShieldCard:
+				this.shield=true;
+				this.value=0;
+				break;
+				
+			case BlessingCard:
+				this.value = -3;
+				break;
+				
+			default:
+				System.out.println("This is not a ritual.");
+				this.value = 0;
+		}
+	}
+	
+	int getValue() {
+		return this.value;
+	}
+	boolean getShield(){return this.shield;}
 }
