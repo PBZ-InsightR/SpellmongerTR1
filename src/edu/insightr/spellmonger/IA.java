c1715ebb5eb22cafe715159c8942e3b3c60c9e1f
crendu <rendu.clement@gmail.com>
2016-12-05 00:47:37 +0100

    mise en forme

--- src/edu/insightr/spellmonger/IA.java
+++ src/edu/insightr/spellmonger/IA.java
@@ -1,59 +1,51 @@
 package edu.insightr.spellmonger;
 import java.util.*;
 
-public class IA
-{
-    private List<Card> liste_de_carte;
-
-    public IA(List<Card> liste_de_carte)
-    {
-        this.liste_de_carte=liste_de_carte;
+public class IA {
+	private List<Card> liste_de_carte;
+	public IA(List<Card> liste_de_carte) {
+		this.liste_de_carte=liste_de_carte;
     }
 
-    Card ChooseBestCard()
-    {
-        if(liste_de_carte.isEmpty())
-        {
-            System.out.println("Plus de cartes ! ");
+	Card ChooseBestCard() {
+		if(liste_de_carte.isEmpty()) {
+			System.out.println("Plus de cartes ! ");
         }
-        int taille = liste_de_carte.size();
-
-        for (int i=0; i<taille ; i++) {
-            if (liste_de_carte.get(i).getId().equals("Curse")) {
-                return liste_de_carte.get(i);
-            }
-        }
-        for (int i=0; i<taille ; i++) {
-            if (liste_de_carte.get(i).getId().equals("Bear")) {
-                return liste_de_carte.get(i);
-            }
-        }
-        for (int i=0; i<taille ; i++) {
-            if (liste_de_carte.get(i).getId().equals("Wolf")) {
-                return liste_de_carte.get(i);
-            }
-        }
-        for (int i=0; i<taille ; i++) {
-            if (liste_de_carte.get(i).getId().equals("Eagle")) {
-                return liste_de_carte.get(i);
-            }
-        }
-        for (int i=0; i<taille ; i++) {
-            if (liste_de_carte.get(i).getId().equals("Shield")) {
-                return liste_de_carte.get(i);
-            }
-        }
-        for (int i=0; i<taille ; i++) {
-            if (liste_de_carte.get(i).getId().equals("Blessing")) {
-                return liste_de_carte.get(i);
-            }
-        }
-
-
+		
+		int taille = liste_de_carte.size();
+		
+		for (int i=0; i<taille ; i++) {
+			if (liste_de_carte.get(i).getId().equals("Curse")) {
+				return liste_de_carte.get(i);
+			}
+		}
+		for (int i=0; i<taille ; i++) {
+			if (liste_de_carte.get(i).getId().equals("Bear")) {
+				return liste_de_carte.get(i);
+			}
+		}
+		for (int i=0; i<taille ; i++) {
+			if (liste_de_carte.get(i).getId().equals("Wolf")) {
+				return liste_de_carte.get(i);
+			}
+		}
+		for (int i=0; i<taille ; i++) {
+			if (liste_de_carte.get(i).getId().equals("Eagle")) {
+				return liste_de_carte.get(i);
+			}
+		}
+		for (int i=0; i<taille ; i++) {
+			if (liste_de_carte.get(i).getId().equals("Shield")) {
+				return liste_de_carte.get(i);
+			}
+		}
+		for (int i=0; i<taille ; i++) {
+			if (liste_de_carte.get(i).getId().equals("Blessing")) {
+				return liste_de_carte.get(i);
+			}
+		}
+		
         System.out.println("ERREUR !!");
         return new Creature("XXX"); //HORREUR ULTIMEEEE
-
-    }
-
-
+	}
 }
