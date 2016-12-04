eff5b008ccd2aaa1ef09b3db11391c54097545bb
crendu <rendu.clement@gmail.com>
2016-12-05 00:46:57 +0100

    mise en forme

--- src/edu/insightr/spellmonger/Card.java
+++ src/edu/insightr/spellmonger/Card.java
@@ -1,19 +1,17 @@
 package edu.insightr.spellmonger;
 
-/*
-** Card abstract class id means the "name" of the card
- */
+/**	Card abstract class id means the "name" of the card	**/
 
 import java.util.Objects;
 
 abstract class Card {
-    private String id;
-
-    Card(String id) {
-        this.id = id;
-    }
-
-    String getId() {
-        return id;
-    }
+	private String id;
+	
+	Card(String id) {
+		this.id = id;
+	}
+	
+	String getId() {
+		return id;
+	}
 }
