dfc6027b70d98096716c8989890b875f69bf53d7
crendu <rendu.clement@gmail.com>
2016-12-06 18:07:57 +0100

    Update SpellmongerApp.java

--- src/edu/insightr/spellmonger/SpellmongerApp.java
+++ src/edu/insightr/spellmonger/SpellmongerApp.java
@@ -13,8 +13,8 @@ public class SpellmongerApp {
 		int roundCounter = 0;
 		boolean jeu_fini = false;
 		
-		Player J1 = new Player("alice", LIFEPOINT ,deck.CreationMain());
-		Player J2 = new Player("bob", LIFEPOINT ,deck.CreationMain());
+		Player J1 = new Player("alice", LIFEPOINT ,deck.DistributionCarte());
+		Player J2 = new Player("bob", LIFEPOINT ,deck.DistributionCarte());
 		
 		do {
 			System.out.println();
