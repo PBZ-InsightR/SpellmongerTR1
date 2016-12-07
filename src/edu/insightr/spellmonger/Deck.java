137351af1e4b5ca6a518aa594ddcdd34febc29df
Benjamin <benjamin.laschkar@devinci.fr>
2016-12-07 18:40:40 +0100

    -New Method with 3 cards -Need to implement how to choose for player

--- src/edu/insightr/spellmonger/Deck.java
+++ src/edu/insightr/spellmonger/Deck.java
@@ -1,143 +1,104 @@
 package edu.insightr.spellmonger;
-import java.util.*;
 
+import java.util.*;
 import static java.util.Collections.shuffle;
 
-/**
- * Created by Chloé +  Dams on 04/10/2016.
- *
- */
+/**	Created by Chloé +  Dams on 04/10/2016.	**/
+
 class Deck {
+    private List<Card> deck;
+    private  int nombreCarte=42;  // permet que chacun des joueurs est 21 cartes
 
-     private List<Card> deck;
-     private  int nombreCarte=42;
-
-    // Un Deck instancié est un deck constitué de 40 cartes et melangé .
-    Deck(){
-
+    // Un Deck instancié est un deck constitué de 42 cartes et melangé.
+    Deck() {
         deck = new ArrayList<>(nombreCarte);
-        deck= CreationDeck();
-        Mix();
+        deck = CreationDeck();
+        shuffle(deck);
     }
 
- public  List<Card> CreationDeck() {
+    public  List<Card> CreationDeck() {
+        Card card;
+        int i;
 
-     Card card;
-     int i;
+        for (i = 0; i < nombreCarte/4; i++) {
+            card = new Creature("Eagle");
+            deck.add(card);
+        }
+        for (i = 0; i < nombreCarte/4; i++) {
+            card = new Creature("Wolf");
+            deck.add(card);
+        }
+        for (i = 0; i < nombreCarte/4; i++) {
+            card = new Creature("Bear");
+            deck.add(card);
+        }
+        for (i = 0; i < nombreCarte/21; i++) {
+            card = new Ritual("Curse");
+            deck.add(card);
+        }
+        for (i = 0; i < (nombreCarte/14); i++) {
+            card = new Ritual("Blessing");
+            deck.add(card);
+        }
+        for (i = 0; i < nombreCarte/8; i++) {
+            card = new Ritual("Shield");
+            deck.add(card);
+        }
+        return  deck;
+    }
 
-     for (i = 0; i < nombreCarte/4; i++) {
-
-         card = new Creature("Eagle");
-         deck.add(card) ;
-     }
-     for (i = 0; i < nombreCarte/4; i++) {
-
-         card = new Creature("Wolf");
-         deck.add(card);
-     }
-     for (i = 0; i < nombreCarte/4; i++) {
-
-         card = new Creature("Bear");
-         deck.add(card);
-     }
-
-     for (i = 0; i < nombreCarte/21; i++) {
-         card = new Ritual("Curse");
-         deck.add(card);
-
-     }
-
-     for (i = 0; i < (nombreCarte/14); i++) {
-         card = new Ritual("Blessing");
-         deck.add(card);
-
-     }
-     for (i = 0; i < nombreCarte/8; i++) {
-         card = new Ritual("Shield");
-         deck.add(card);
-
-     }
-    return  deck;
- }
-
- public void Mix(){
-
-     shuffle(deck);
- }
-
-
-    //Permet à un joueur de recuperer une main de 21 cartes.
-
+    // Distribue à chaque joueur une deck de 21 cartes.
     public List<Card> DistributionCarte() {
         Card cartePioche = null;
-        List<Card> deckJoueur= new ArrayList<>(nombreCarte/2);
+        List<Card> deckJoueur = new ArrayList<>(nombreCarte/2);
+
         if (deck.size() != 0) {
-            for (int i = 0; i < nombreCarte/2; i++)
-            {
+            for (int i = 0; i < nombreCarte/2; i++) {
                 try {
-                cartePioche = deck.get(0);
-                deck.remove(0);
+                    cartePioche = deck.get(0);
+                    deck.remove(0);
                 }
                 catch (IndexOutOfBoundsException e){}
                 deckJoueur.add(cartePioche);
             }
-
         } else {
             System.out.println("Deck vide");
-            deckJoueur=null;
+            deckJoueur = null;
         }
         return deckJoueur;
-
-
     }
 
-    public List<Card> CreationMain(List<Card>deckJoueur){
+    // Distribue à chaque joueur une main de 3 cartes.
+    public List<Card> CreationMain(List<Card> deckJoueur){
+        Card cartePiocheDeckJoueur = null;
+        List<Card> mainActuelle = new ArrayList<>(3);
 
-        Card cartePiocheDeckJoueur=null;
-        List<Card>mainActuelle= new ArrayList<>(3);
-
-        if ( mainActuelle.size()==0)
-            for (int i =0; i<3; i++) {
+        if ( mainActuelle.size() == 0)
+            for (int i =0; i < 3; i++) {
                 try {
-                    cartePiocheDeckJoueur=deckJoueur.get(0);
+                    cartePiocheDeckJoueur = deckJoueur.get(0);
                     deckJoueur.remove(0);
                     mainActuelle.add(cartePiocheDeckJoueur);
                 }
                 catch (IndexOutOfBoundsException e){
-            }
-
-        }
-        else {
+                }
+            } else {
             System.out.println("Main actuelle non vide");
-
         }
-
         return mainActuelle;
     }
 
-
-
-
-
-
     // Fonction qui lorsqu'on lui donne une main affiche le contenu de la main du joueur concerné.
-
-   public void AfficherMain(List<Card> Une_main ) {
-
+    public void AfficherMain(List<Card> Une_main ) {
         // Initialisation de la Map
         Map<String, Integer> map = new HashMap<>();
         String[] tab = {"Eagle" , "Wolf" , "Bear" , "Curse" , "Blessing" , "Shield"};
 
         for (String carte : tab) {
-
             map.put(carte,0);
         }
-
-
         for (Card card : Une_main) {
-
             switch (card.getId()) {
-
                 case "Eagle":
                     map.put("Eagle", map.get("Eagle") + 1);
                     break;
@@ -148,7 +109,8 @@ class Deck {
 
                 case "Bear":
                     map.put("Bear", map.get("Bear") + 1);
-                     break;
+                    break;
+
                 case "Curse":
                     map.put("Curse", map.get("Curse") + 1);
                     break;
@@ -160,32 +122,23 @@ class Deck {
                 case "Shield":
                     map.put("Shield", map.get("Shield") + 1);
                     break;
-
             }
         }
 
-        System.out.print(
-                "This is your hand : \n " +
-                        "Eagle = " + map.get("Eagle") + "\n"+
-                        "Wolf = " + map.get("Wolf") + "\n"+
-                        "Bear = " + map.get("Bear") +"\n"+
-                        "Curse = " + map.get("Curse") +"\n"+
-                        "Blessing = " + map.get("Blessing")+ "\n"+
-                        "Shield =  " + map.get("Shield")
+        System.out.print("This is your hand : \n " +
+                "Eagle = " + map.get("Eagle") + "\n" +
+                "Wolf = " + map.get("Wolf") + "\n" +
+                "Bear = " + map.get("Bear") +"\n" +
+                "Curse = " + map.get("Curse") +"\n" +
+                "Blessing = " + map.get("Blessing")+ "\n" +
+                "Shield =  " + map.get("Shield")
         );
     }
 
-
-
-       public Card PickCard(List<Card> Une_main , int i)
-        {
-           Card carte_choisit;
-           carte_choisit = Une_main.get(i);
-           Une_main.remove(i);
-           return carte_choisit;
-        }
-
+    public Card PickCard(List<Card> Une_main , int i) {
+        Card carte_choisit;
+        Une_main.remove(i);
+        carte_choisit = Une_main.get(i);
+        return carte_choisit;
     }
-
-
-
+}
--- src/edu/insightr/spellmonger/SpellmongerApp.java
+++ src/edu/insightr/spellmonger/SpellmongerApp.java
@@ -1,125 +1,128 @@
 package edu.insightr.spellmonger;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.*;
+
 
 public class SpellmongerApp {
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Deck deck = new Deck();
-        final int NOMBREDECARTE = 20;
+        final int NOMBREDECARTE = 21;
         final int LIFEPOINT = 20;
         int roundCounter = 0;
         boolean jeu_fini = false;
+        int NOMBREDECARTEMAIN=3;
+        List<Card> deckJ1 = deck.DistributionCarte(); //Creation du Deck J1
+        List<Card> deckJ2 = deck.DistributionCarte(); //Creation du Deck J2
 
-        Player J1 = new Player("alice", LIFEPOINT ,deck.DistributionCarte());
-        Player J2 = new Player("bob", LIFEPOINT ,deck.DistributionCarte());
 
-        do  {
+        Deck defausse = new Deck();
+        List<Card> defausseJ1=defausse.CreationDeck();
+        List<Card> defausseJ2=defausse.CreationDeck();
 
+        List<Card> mainJ1 = deck.CreationMain(deckJ1); //Main de départ J1
+        List<Card> mainJ2 = deck.CreationMain(deckJ2); //Main de depart J2
+
+        Player J1 = new Player("alice", LIFEPOINT ,deckJ1, mainJ1 ); //Creation des joueurs
+        Player J2 = new Player("bob", LIFEPOINT ,deckJ2, mainJ2);
+
+
+
+        do {
             System.out.println();
-            System.out.println("Entering round " + roundCounter + "..."); //INFO
+            System.out.println("Entering round " + roundCounter + "..."); //INFO ROUND
 
-            if (roundCounter==NOMBREDECARTE)
-            {
-                if (J1.getPv()>J2.getPv())
-                {
-                    System.out.println(J1.getName() +" win because he got more life point and there is no more cards ! ");
-                }
-              if(J2.getPv()>J1.getPv())
-                {
-                    System.out.println(J2.getName() +" win because he got more life point and there is no more cards ! ");
-                }
-               if(J2.getPv()==J1.getPv())
-               {
-                   System.out.println(" Epic DRAW because same life point and there is no more cards ! ");
-               }
-                break;
-            }
-            //IMPLEMENTER L'IA ICI
-            IA IA_J1= new IA(J1.getDeckDuJoueur());
+            //L'ia prend le controle de la main du J1
+            IA IA_J1= new IA(J1.GetMainDuJoueur());
+            //Methode pour choisir la meilleur carte parmis les 3
             Card card1 = IA_J1.ChooseBestCard();
             //Card card1 = J1.getDeckDuJoueur().get(roundCounter); //Pas encore d'ia, donc le joueur joue la carte 1 au t1, la carte 2 au t2, etc ..
-            Card card2 = J2.getDeckDuJoueur().get(0);
-/*
-            System.out.println("/n Main de alice :");
-            deck.AfficherMain(J1.getDeckDuJoueur()); //On affiche les main des joueurs. Mais prend bcp de place dans les logs
-            System.out.println("/n Main de bob :");
-            deck.AfficherMain(J2.getDeckDuJoueur());
-*/
+
+            Card card2 = J2.GetMainDuJoueur().get(0); //Le joueur prend la première carte de sa main
+
+			/* System.out.println("/n Main de alice :");
+			* deck.AfficherMain(J1.getDeckDuJoueur()); //On affiche les main des joueurs. Mais prend bcp de place dans les logs
+			* System.out.println("/n Main de bob :");
+			* deck.AfficherMain(J2.getDeckDuJoueur());*/
+
             System.out.println(J1.getName()+" draw a "+ card1.getId()); //INFO
             System.out.println(J2.getName()+" draw a "+ card2.getId()); //INFO
-              /*
-             4 cas  sont possible ici ! Rituel vs Rituel,
-                                          Crea vs Crea,
-                                          Rituel vs Crea
-                                          Crea vs Rituel.
-               On gere tout les cas possibles.
 
-              */
-            if(card1 instanceof Creature && card2 instanceof Creature )
-            {
+			/* 4 cas  sont possible ici ! Rituel vs Rituel,
+			*                            Crea vs Crea,
+			*                            Rituel vs Crea
+			*                            Crea vs Rituel.
+			* On gere tout les cas possibles.*/
+
+            if(card1 instanceof Creature && card2 instanceof Creature ) {
                 J2.creaVsCrea(card1,card2);
                 J1.creaVsCrea(card2,card1);
             }
-           if(card1 instanceof Ritual  &&  card2 instanceof Ritual )
-            {
-               J1.rituVsRitu(card1,card2);
-               J2.rituVsRitu(card2,card1);
-
+            if(card1 instanceof Ritual  &&  card2 instanceof Ritual ) {
+                J1.rituVsRitu(card1,card2);
+                J2.rituVsRitu(card2,card1);
             }
-            if(card1 instanceof Ritual  &&  card2 instanceof Creature )
-            {
+            if(card1 instanceof Ritual  &&  card2 instanceof Creature ) {
                 J1.CreaVsRituJ1(card1,card2);
                 J2.CreaVsRituJ2(card1,card2);
-
             }
-            if(card2 instanceof Ritual  &&  card1 instanceof Creature )
-            {
+            if(card2 instanceof Ritual  &&  card1 instanceof Creature ) {
                 J2.CreaVsRituJ1(card2,card1);
                 J1.CreaVsRituJ2(card2,card1);
             }
 
-            J1.getDeckDuJoueur().remove(card1);
-            J2.getDeckDuJoueur().remove(card2);
+
+            // Defausse (de la main)
+            defausseJ1.add(card1);
+            defausseJ2.add(card2);
+
+            J1.GetMainDuJoueur().remove(card1);
+            J2.GetMainDuJoueur().remove(card2);
+
+            NOMBREDECARTEMAIN--;
+
+            if (J1.getDeckDuJoueur().size()==0)
+            {
+                deckJ1=defausseJ1;
+                deckJ2=defausseJ2;
+                J1.SetDeckDuJoueur(deckJ1);
+                J2.SetDeckDuJoueur(deckJ2);
+            }
+
+            if (J1.GetMainDuJoueur().size()==0)
+            {
+                System.out.println(J1.getName() +" and "+ J2.getName()  +" draw 3 cards !");
+                J1.SetMainDuJoueur(deck.CreationMain(deckJ1));
+                J2.SetMainDuJoueur(deck.CreationMain(deckJ2));
+                NOMBREDECARTEMAIN=3;
+            }
 
             //Affichage des points de vie des joueurs
             System.out.println(J1.getName() +" got "+J1.getPv()+" life points");
             System.out.println(J2.getName() +" got "+J2.getPv()+" life points");
 
             //3 cas possible pour la victoire ou l'egalite
-            if(J1.estMort() && J2.estEnVie())
-            {
+            if(J1.estMort() && J2.estEnVie()) {
                 jeu_fini = true;
                 System.out.println(J1.getName() + " has " + J1.getPv() + " life points, he is dead, congrats to "+ J2.getName());
             }
-            if(J2.estMort() && J1.estEnVie())
-            {
+            if(J2.estMort() && J1.estEnVie()) {
                 jeu_fini = true;
                 System.out.println(J2.getName() + " has " + J2.getPv() + " life points, he is dead, congrats to "+ J1.getName());
             }
-            if(J2.estMort() && J1.estMort())
-            {
+            if(J2.estMort() && J1.estMort()) {
                 jeu_fini = true;
-                if(J1.getPv()>J2.getPv())
-                {
+                if(J1.getPv()>J2.getPv()) {
                     System.out.println(J1.getName() + " and  " + J2.getName() + " are deads but "+ J1.getName() + " got more hp with "+ J1.getPv()+" against "+ J2.getPv() +" for "+J2.getName()+" so "+J1.getName()+" win !");
-                }
-                else
-                {
+                } else {
                     System.out.println(J2.getName() + " and  " + J1.getName() + " are deads but "+ J2.getName() + " got more hp with "+ J2.getPv()+" against "+ J1.getPv() +" for "+J1.getName()+" so "+J2.getName()+" win !");
                 }
             }
-
             roundCounter++;
         } while (!jeu_fini);
-
     }
-
-
-
-
-
-
-
-
 }
