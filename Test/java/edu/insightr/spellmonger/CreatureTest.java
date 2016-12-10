9841910ef15fda6445d0179ef25b37a9e4518d4d
Damien Rubal <damien.rubal@devinci.fr>
2016-12-10 17:04:05 +0100

    Test sur la methode GetDamage().

--- Test/java/edu/insightr/spellmonger/CreatureTest.java
+++ Test/java/edu/insightr/spellmonger/CreatureTest.java
@@ -1,6 +1,6 @@
 package edu.insightr.spellmonger;
 
-import org.junit.Test;
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 
@@ -8,16 +8,27 @@ import static org.junit.Assert.*;
  * Created by Dam's on 07/12/2016.
  */
 public class CreatureTest {
-    @Test
-    public void getDamage() throws Exception { 
+    Object o;
+
+
+
+    @org.junit.Before
+    public void AuDebut() throws Exception {
+        o = null;
 
     }
 
+    @org.junit.Test
+    public void getDamage() throws Exception {
+        // assertEquals("il faut 42 cartes",1,2);
+        Creature creatureEagle = new Creature("Eagle");
+        Creature creatureWolf = new Creature("Wolf");
+        Creature creatureBear = new Creature("Bear");
 
-
-    @Test
-    public void getId() throws Exception {
-
+        assertEquals(" attacks of an eagle ", 1, creatureEagle.getDamage());
+        assertEquals("attacks of an wolf",2,creatureWolf.getDamage());
+        assertEquals("attacks of an bear  ",3,creatureBear.getDamage());
+        // assertTrue( );
     }
 
 }
---
+++ Test/java/edu/insightr/spellmonger/DeckTest.java
@@ -0,0 +1,22 @@
+package edu.insightr.spellmonger;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dam's on 07/12/2016.
+ */
+public class DeckTest {
+
+    private List<Card> deckJoueur;
+    @Test
+    public void creationMain() throws Exception {
+
+
+    }
+
+}
