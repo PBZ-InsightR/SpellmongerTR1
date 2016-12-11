0492cd072344d737f70ca3badd5e5b8362f85c4b
Damien Rubal <damien.rubal@devinci.fr>
2016-12-11 12:52:45 +0100

    PlayerTest / methode damage()

--- Test/java/edu/insightr/spellmonger/PlayerTest.java
+++ Test/java/edu/insightr/spellmonger/PlayerTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+
 /**
  * Created by Dam's on 11/12/2016.
  */
@@ -15,14 +16,25 @@ public class PlayerTest {
     @Before
     public void Init()
     {
-        Deck deck1 = new Deck();
-        List<Card> deckplayer1=deck1.GetDeck();
-        List<Card> handPlayer1=deck1.GetMainactuelle();
-        Player player1 = new Player("player1",20,deckplayer1,handPlayer1);
-        
+
+
     }
     @Test
     public void damage() throws Exception {
+        Deck deck1 = new Deck();
+        List<Card> deckplayer1=deck1.GetDeck();
+        List<Card> handPlayer1=deck1.GetMainactuelle();
+        Player player1 = new Player("Player1",20,deckplayer1,handPlayer1);
+
+        int pv = 0 ;
+        pv = player1.getPv()-3;
+        assertEquals("pv(20) - 3 ",17,pv);
+
+        pv = player1.getPv()-0;
+        assertEquals("pv(20)-0",20,pv);
+
+        pv = player1.getPv()+3;
+        assertEquals("pv(20)+3",23,pv);
 
     }
 
