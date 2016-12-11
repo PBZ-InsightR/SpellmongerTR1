d1d752f131cb1f66b9a0d7c66570e3fb9771f489
Damien Rubal <damien.rubal@devinci.fr>
2016-12-11 11:52:02 +0100

    RitualTest

--- Test/java/edu/insightr/spellmonger/CreatureTest.java
+++ Test/java/edu/insightr/spellmonger/CreatureTest.java
@@ -11,16 +11,9 @@ public class CreatureTest {
 
 
 
-
-    /*@org.junit.Before
-    public void AuDebut() throws Exception {
-        o = null;
-
-    }*/
-
     @org.junit.Test
     public void getDamage() throws Exception {
-        // assertEquals("il faut 42 cartes",1,2);
+        
         Creature creatureEagle = new Creature("Eagle");
         Creature creatureWolf = new Creature("Wolf");
         Creature creatureBear = new Creature("Bear");
