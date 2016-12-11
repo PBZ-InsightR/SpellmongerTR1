bd865700881687b8ea99855dfff69e3a991b9ef7
Damien Rubal <damien.rubal@devinci.fr>
2016-12-11 01:45:09 +0100

    IATest, PlayerTest , RitualTest

---
+++ Test/java/edu/insightr/spellmonger/PlayerTest.java
@@ -0,0 +1,46 @@
+package edu.insightr.spellmonger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dam's on 11/12/2016.
+ */
+public class PlayerTest {
+    @Test
+    public void damage() throws Exception {
+
+    }
+
+    @Test
+    public void estMort() throws Exception {
+
+    }
+
+    @Test
+    public void estEnVie() throws Exception {
+
+    }
+
+    @Test
+    public void creaVsCrea() throws Exception {
+
+    }
+
+    @Test
+    public void rituVsRitu() throws Exception {
+
+    }
+
+    @Test
+    public void creaVsRituJ1() throws Exception {
+
+    }
+
+    @Test
+    public void creaVsRituJ2() throws Exception {
+
+    }
+
+}
---
+++ Test/java/edu/insightr/spellmonger/RitualTest.java
@@ -0,0 +1,29 @@
+package edu.insightr.spellmonger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dam's on 11/12/2016.
+ */
+public class RitualTest {
+
+
+
+    @Test
+    public void getValue() throws Exception {
+
+    }
+
+    @Test
+    public void getShield() throws Exception {
+
+    }
+
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+}
