5832aa890150b5da1049df0947785170708515b7
Alison <alison.rakotomahefa@devinci.fr>
2016-12-13 23:13:33 +0100

    essai

--- Test/java/edu/insightr/spellmonger/GameStepdefs.java
+++ Test/java/edu/insightr/spellmonger/GameStepdefs.java
@@ -1,8 +1,19 @@
 package edu.insightr.spellmonger;
 
-import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import org.junit.Assert.*;
+import cucumber.api.java.en.When;
+import edu.insightr.spellmonger.model.Card;
+import edu.insightr.spellmonger.model.Creature;
+import edu.insightr.spellmonger.model.Player;
+import edu.insightr.spellmonger.model.SpellmongerApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 /**
@@ -14,7 +25,11 @@ public class GameStepdefs {
         // arg(0) means the player
         // arg(1) means the point number
         // arg(2) means the number of card in their deck
-
+        List<Card> hand = new ArrayList<>(arg2);
+        Player player = app.getPlayer1();
+        if ("Alice".equals(player.getName())) {
+            assertThat(player.getName(), is(equalTo(arg0)));
+            assertThat(player.getPv(), is(equalTo(arg1)));
         
         throw new PendingException();
     }
