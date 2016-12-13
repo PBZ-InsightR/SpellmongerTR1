61782857a3fa89a89bffd169f7e11267c7b136c7
Alison <alison.rakotomahefa@devinci.fr>
2016-12-13 23:33:08 +0100

    essai

--- Test/java/edu/insightr/spellmonger/GameStepdefs.java
+++ Test/java/edu/insightr/spellmonger/GameStepdefs.java
@@ -1,12 +1,6 @@
 package edu.insightr.spellmonger;
 
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import edu.insightr.spellmonger.model.Card;
-import edu.insightr.spellmonger.model.Creature;
-import edu.insightr.spellmonger.model.Player;
-import edu.insightr.spellmonger.model.SpellmongerApp;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,17 +14,32 @@ import static org.junit.Assert.assertThat;
  * Test in a simple scenario(initialisation) with cucumber
  */
 public class GameStepdefs {
-    @Then("^\"([^\"]*)\" has (\\d+) points and (\\d+) cards in her deck$")
+    @Then("Alice has (\\d+) points and (\\d+) cards in her deck$")
     public void hasPointsAndCardsInHerDeck(String arg0, int arg1, int arg2) throws Throwable {
         // arg(0) means the player
         // arg(1) means the point number
         // arg(2) means the number of card in their deck
-        List<Card> hand = new ArrayList<>(arg2);
-        Player player = app.getPlayer1();
+
+        Player player = new Player("Alice");
         if ("Alice".equals(player.getName())) {
             assertThat(player.getName(), is(equalTo(arg0)));
             assertThat(player.getPv(), is(equalTo(arg1)));
-        
-        throw new PendingException();
+            assertThat(player.getDeckDuJoueur(), is(equalTo(arg2)));
+
+            //throw new PendingException();
+        }
+    }
+
+    @Then("Bob has (\\d+) points and (\\d+) cards in her deck$")
+    public void hasPointsAndCardsInHerDeck(String arg0, int arg1, int arg2) throws Throwable {
+        // arg(0) means the player
+        // arg(1) means the point number
+        // arg(2) means the number of card in their deck
+        Player player = new Player("Bob");
+        if ("Bob".equals(player.getName())) {
+            assertThat(player.getName(), is(equalTo(arg0)));
+            assertThat(player.getPv(), is(equalTo(arg1)));
+            assertThat(player.getDeckDuJoueur(), is(equalTo(arg2)));
+        }
     }
 }
