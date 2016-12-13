a86dcda060715c53833e9af1eaa27f87b0ac244b
Damien Rubal <damien.rubal@devinci.fr>
2016-12-13 22:17:56 +0100

    Mis à jour de PlayerTest ( avec la disparition de "mainActuelle " comme argument dans la classe Deck , ça a changé bcq de choses dans les tests )

--- Test/java/edu/insightr/spellmonger/PlayerTest.java
+++ Test/java/edu/insightr/spellmonger/PlayerTest.java
@@ -23,8 +23,8 @@ public class PlayerTest {
     public void damage() throws Exception {
         Deck deck1 = new Deck();
         List<Card> deckplayer1=deck1.GetDeck();
-        List<Card> handPlayer1=deck1.GetMainactuelle();
-        Player player1 = new Player("Player1",20,deckplayer1,handPlayer1);
+
+        Player player1 = new Player("Player1",20,deckplayer1);
 
         int pv = 0 ;
         pv = player1.getPv()-3;
@@ -41,11 +41,11 @@ public class PlayerTest {
     public void creaVsCrea() throws Exception {
 
         int damageTest =0;
-        int damageTest2=0;
-        Card card1 = new Card ("Eagle");
-        Card card2 = new Card ("wolf");
 
-        damageTest=(card1.getDamage())-((card2.getDamage));
+        Card wolf = new Creature("Wolf");
+        Card eagle= new Creature ("Eagle");
+
+        damageTest= ((Creature)eagle).getDamage()-((Creature)wolf).getDamage();
         assertEquals(1,damageTest);
 
     }
@@ -53,9 +53,9 @@ public class PlayerTest {
     public void estMort() throws Exception {
         Deck deck = new Deck();
         List<Card> deckPlayer = deck.GetDeck();
-        List<Card> hand = deck.GetMainactuelle();
+
         int lifePoint = 10;
-        Player P = new Player("PlayerTestEstMort", lifePoint, deckPlayer, hand );
+        Player P = new Player("PlayerTestEstMort", lifePoint, deckPlayer );
 
         // Case where Player is alive
         //Should return false
@@ -66,7 +66,7 @@ public class PlayerTest {
         //Should return true
         lifePoint =0;
         expected = true;
-        Player P_dead = new Player("PlayerDead", lifePoint, deck.DistributionCarte(),hand );
+        Player P_dead = new Player("PlayerDead", lifePoint, deck.DistributionCarte() );
         assertEquals("Player is dead ", expected, P_dead.estMort());
     }
 
@@ -74,9 +74,9 @@ public class PlayerTest {
     public void estEnVie() throws Exception {
         Deck deck = new Deck();
         List<Card> deckPlayer = deck.GetDeck();
-        List<Card> hand = deck.GetMainactuelle();
+
         int lifePoint = 10;
-        Player P = new Player("PlayerTest EstEnVie", lifePoint, deckPlayer, hand );
+        Player P = new Player("PlayerTest EstEnVie", lifePoint, deckPlayer);
 
         //Case where Player is alive
         //Should return true
@@ -87,7 +87,7 @@ public class PlayerTest {
         //Should return false
         lifePoint = 0;
         expected = false;
-        Player P_dead = new Player("PlayerDead", lifePoint, deck.DistributionCarte(),hand );
+        Player P_dead = new Player("PlayerDead", lifePoint, deck.DistributionCarte() );
         assertEquals("Player is dead ", expected, P_dead.estEnVie());
     }
 
@@ -101,13 +101,9 @@ public class PlayerTest {
         Ritual blessing=new Ritual("Blessing");
         int damageTest=0;
 
-        /*boolean shield =true;
-        damageTest=shield.getValue();
 
-        assertEquals(); */
-
-        damage curseTest=curse.getValue();
-        damage blessingTest=blessing.getValue();
+        int curseTest=curse.getValue();
+        int blessingTest=blessing.getValue();
 
         assertEquals(+3,curseTest);
         assertEquals(-3,blessingTest);
@@ -118,11 +114,11 @@ public class PlayerTest {
     @Test
     public void creaVsRituJ1() throws Exception {
 
-        Card card1 = new Card("Wolf");
-        Rituel curse= new Curse("Curse");
+        Card card1 = new Creature("Wolf");
+        Ritual curse= new Ritual("Curse");
 
-        boolean curse=true;
-        damageTest= card1.getDamage();
+        boolean bool=true;
+        int damageTest= ((Creature)card1).getDamage();
 
         assertEquals(2,damageTest);
 
@@ -137,9 +133,9 @@ public class PlayerTest {
     public void getPvTest() throws Exception {
         Deck deck = new Deck();
         List<Card> deckP = deck.GetDeck();
-        List<Card> handP = deck.GetMainactuelle();
+
         int lifePoint = 20 ,expected = 20;
-        Player P = new Player("PlayerTest",lifePoint, deckP,handP);
+        Player P = new Player("PlayerTest",lifePoint, deckP);
 
         assertEquals("Player P has 20 life's point", expected, P.getPv());
     }
